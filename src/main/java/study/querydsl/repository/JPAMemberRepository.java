cc662e53ae5a58d0bd7180e0cd62abb7e35d92b7
zerosouth <yn782@naver.com>
2021-03-28 23:07:43 +0900

    21.03.28

--- src/main/java/study/querydsl/repository/JPAMemberRepository.java
+++ src/main/java/study/querydsl/repository/JPAMemberRepository.java
@@ -1,6 +1,8 @@
 package study.querydsl.repository;
 
 import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
@@ -90,4 +92,56 @@ public class JPAMemberRepository {
                 .where(builder)
                 .fetch();
     }
+
+    public List<MemberTeamDto> search(MemberSearchCondition condition){
+        BooleanBuilder builder = new BooleanBuilder();
+        if(hasText(condition.getUsername())){
+            builder.and(member.username.eq((condition.getUsername())));
+        }
+        if(hasText(condition.getTeamName())){
+            builder.and(team.name.eq((condition.getTeamName())));
+        }
+        if(condition.getAgeGoe()!=null){
+            builder.and(member.age.goe((condition.getAgeGoe())));
+        }
+        if(condition.getAgeLoe()!=null){
+            builder.and(member.age.loe((condition.getAgeLoe())));
+        }
+
+        return queryFactory
+                .select(new QMemberTeamDto(
+                        member.id.as("memberId"),
+                        member.username,
+                        member.age,
+                        team.id.as("teamId"),
+                        team.name.as("teamName")))
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe())
+                        )
+                .fetch();
+    }
+
+    private BooleanExpression usernameEq(String username) {
+    return hasText(username) ? member.username.eq(username) : null;
+    }
+    private BooleanExpression teamNameEq(String teamName) {
+        return hasText(teamName) ? member.team.name.eq(teamName) : null;
+    }
+
+    private BooleanExpression ageGoe(Integer ageGoe) {
+        return ageGoe == null ? null : member.age.goe(ageGoe);
+
+    }
+
+    private BooleanExpression ageLoe(Integer ageLoe) {
+        return ageLoe == null ? null : member.age.loe(ageLoe);
+    }
+
+
+
+
 }
--- src/test/java/study/querydsl/MemberJPARepositoryTest.java
+++ src/test/java/study/querydsl/MemberJPARepositoryTest.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.dto.MemberSearchCondition;
+import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.entity.Member;
+import study.querydsl.entity.Team;
 import study.querydsl.repository.JPAMemberRepository;
 
 import javax.persistence.EntityManager;
@@ -20,7 +23,7 @@ import static org.assertj.core.api.Assertions.*;
 public class MemberJPARepositoryTest {
 
     @Autowired
-    EntityManager em ;
+    EntityManager em;
 
     @Autowired
     JPAMemberRepository memberJPARepository;
@@ -30,7 +33,7 @@ public class MemberJPARepositoryTest {
     @Transactional
     @Rollback(false)
     public void save() throws Exception {
-        Member member1 = new Member("member",10);
+        Member member1 = new Member("member", 10);
         memberJPARepository.save(member1);
 
         Member findMember = memberJPARepository.findById(member1.getId()).get();
@@ -39,7 +42,7 @@ public class MemberJPARepositoryTest {
         List<Member> result1 = memberJPARepository.findAll();
         assertThat(result1).containsExactly(member1);
 
-        List<Member> result2= memberJPARepository.findByUsername("member");
+        List<Member> result2 = memberJPARepository.findByUsername("member");
         assertThat(result2).containsExactly(member1);
     }
 
@@ -47,7 +50,7 @@ public class MemberJPARepositoryTest {
     @Transactional
     @Rollback(false)
     public void querydslTest() throws Exception {
-        Member member1 = new Member("member",10);
+        Member member1 = new Member("member", 10);
         memberJPARepository.save(member1);
 
         Member findMember = memberJPARepository.findById(member1.getId()).get();
@@ -56,7 +59,36 @@ public class MemberJPARepositoryTest {
         List<Member> result1 = memberJPARepository.findAll_Querydsl();
         assertThat(result1).containsExactly(member1);
 
-        List<Member> result2= memberJPARepository.findByUsername_Querydsl("member");
+        List<Member> result2 = memberJPARepository.findByUsername_Querydsl("member");
         assertThat(result2).containsExactly(member1);
+    }
+
+    @Test
+    @Transactional
+//    @Rollback(false)
+    public void SearchTest() throws Exception {
+        Team teamA = new Team("TeamA");
+        Team teamB = new Team("TeamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        MemberSearchCondition condition = new MemberSearchCondition();
+        condition.setAgeGoe(35);
+        condition.setAgeLoe(40);
+        condition.setTeamName("TeamB");
+        List<MemberTeamDto> result = memberJPARepository.search(condition);
+//        List<MemberTeamDto> result = memberJPARepository.searchBuilder(condition);
+
+        assertThat(result).extracting("username").containsExactly("member4");
     }
 }
